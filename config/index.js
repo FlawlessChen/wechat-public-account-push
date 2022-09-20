c79c798402480b246b7e2842dd84eff7d276f6a9
chenqingfei <58811727+FlawlessChen@users.noreply.github.com>
2022-09-20 16:58:21 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -64,7 +64,7 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "oNCQI6GCvpy3TMw9wW5CTFnAiMh0",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "_OPtD03CtkfcjaQFwkTfwAtkCN0Xwlp9cdpHT5Humi4",
+      useTemplateId: "2wllZv1LgEQKXAZy-ElKe_jc6foQc_KoVKZkRWQMS9A",
       // 所在省份
       province: "山东",
       // 所在城市
@@ -144,9 +144,9 @@ export const config = {
       // 所在省份
       province: "山东",
       // 所在城市
-      city: "德州",
+      city: "济南",
       // 新历生日, 仅用作获取星座运势, 格式必须
-      horoscopeDate: '02-28',
+      horoscopeDate: '05-01',
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
       horoscopeDateType: '今日',
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
